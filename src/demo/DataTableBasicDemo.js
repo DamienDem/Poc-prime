4794f7866f42a323aad02420f69c6d6bd3b805db
DamienDem <84338924+DamienDem@users.noreply.github.com>
2022-08-08 09:20:43 +0200

    Initial commit

---
+++ src/demo/DataTableBasicDemo.js
@@ -0,0 +1,36 @@
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import '../../index.css';
+import ReactDOM from 'react-dom';
+
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProductService } from '../service/ProductService';
+
+const DataTableBasicDemo = () => {
+    const [products, setProducts] = useState([]);
+    const productService = new ProductService();
+
+    useEffect(() => {
+        productService.getProductsSmall().then(data => setProducts(data));
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <div className="card">
+                <DataTable value={products} responsiveLayout="scroll">
+                    <Column field="code" header="Code"></Column>
+                    <Column field="name" header="Name"></Column>
+                    <Column field="category" header="Category"></Column>
+                    <Column field="quantity" header="Quantity"></Column>
+                </DataTable>
+            </div>
+        </div>
+    );
+}
+                
+const rootElement = document.getElementById("root");
+ReactDOM.render(<DataTableBasicDemo />, rootElement);
